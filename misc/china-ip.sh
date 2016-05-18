5ccf2f37d6afe8f56371130463bfa3cbb96da4cb
m31271n <m31271n@gmail.com>
2016-05-19 02:18:56 +0800

    Make china-ip.sh pipe to directory rules

--- misc/china-ip.sh
+++ misc/china-ip.sh
@@ -3,6 +3,6 @@
 
 wget -t9 -T5 http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest -O /tmp/china-ip.tmp
 if grep ipv4 /tmp/china-ip.tmp >/dev/null; then
-    cat /tmp/china-ip.tmp| awk -F\| '/CN\|ipv4/ { printf("%s/%d\n", $4, 32-log($5)/log(2)) }' > chinaip
+    cat /tmp/china-ip.tmp| awk -F\| '/CN\|ipv4/ { printf("%s/%d\n", $4, 32-log($5)/log(2)) }' > ../rules/GeoIP-CN
 fi
 rm -f /tmp/china-ip.tmp
