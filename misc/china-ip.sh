45a40af0454da203b3afdf28b73b519d4e07e61b
m31271n <m31271n@gmail.com>
2016-05-19 22:45:49 +0800

    Update geoChecker with new rules

--- misc/china-ip.sh
+++ misc/china-ip.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-# This script will be used later
 
-wget -t9 -T5 http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest -O /tmp/china-ip.tmp
-if grep ipv4 /tmp/china-ip.tmp >/dev/null; then
-    cat /tmp/china-ip.tmp| awk -F\| '/CN\|ipv4/ { printf("%s/%d\n", $4, 32-log($5)/log(2)) }' > ../rules/GeoIP-CN
-fi
-rm -f /tmp/china-ip.tmp
+wget -t9 -T5 http://www.ipdeny.com/ipblocks/data/aggregated/cn-aggregated.zone > ../rules/GeoIP-CN
