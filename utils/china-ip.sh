4393690aaae139eb5b7d268752cf946e97a8904a
m31271n <m31271n@gmail.com>
2017-01-10 23:00:38 +0800

    Fix several stupid bugs

--- utils/china-ip.sh
+++ utils/china-ip.sh
@@ -2,4 +2,4 @@
 
 DIR=$( cd "$(dirname "$BASH_SOURCE[0]")" ;pwd -P )
 
-wget -t9 -T5 http://www.ipdeny.com/ipblocks/data/aggregated/cn-aggregated.zone -O - > ${DIR}/../rules/GeoIP-CN
+wget -t9 -T5 http://www.ipdeny.com/ipblocks/data/aggregated/cn-aggregated.zone -O - > ${DIR}/../rules/IP
